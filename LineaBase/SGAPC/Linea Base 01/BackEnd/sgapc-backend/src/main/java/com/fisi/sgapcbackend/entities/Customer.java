ad48ce7dad81e72c7cb66e444cbcbe658f37d6a6
Elton Rodriguez <elton.rodriguez@unmsm.edu.pe>
2022-06-29 12:56:58 -0500

    Create Customer.java

---
+++ LineaBase/SGAPC/Linea Base 01/BackEnd/sgapc-backend/src/main/java/com/fisi/sgapcbackend/entities/Customer.java
@@ -0,0 +1,53 @@
+package com.fisi.sgapcbackend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 120)
+    private String firstname;
+
+    @Column(length = 120)
+    private String lastname;
+
+    @Column(length = 30)
+    private String telephone;
+
+    @Column(length = 60)
+    private String address;
+
+    
+    @Column(unique = true, length = 12)
+    private String ruc;
+    
+    @Column(unique = true, length = 12)
+    private String dni;
+
+    @NotEmpty(message = "no puede estar vacio")
+    @Email(message = "no es una dirección de correo bien formada")
+    @Column(nullable = false, unique = true, length = 60)
+    private String email;
+
+    @Column(name = "create_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createAt;
+
+    @Column(name = "update_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime updateAt;
+}
